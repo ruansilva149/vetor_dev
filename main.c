6062bcfd6d3ad848ab1b8acf59ad6ae847e16ba2
Ruan Silva <ruan.silva149@etec.sp.gov.br>
2023-11-08 13:07:49 -0300

    toques finais

--- main.c
+++ main.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int comparacao(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
-
 int main() {
     int vetor[7];
-	int i;
-	
-    for (i = i < 0; i < 7; i++) {
-        printf("Digite o numero para a posicao do vetor %d: ", i);
+    int i, e, temp;
+
+
+    for (i = 0; i < 7; i++) {
+        printf("Digite um numero %d: ", i);
         scanf("%d", &vetor[i]);
         vetor[i] *= 2;
-	}
-	
-	qsort(vetor, 7, sizeof(int), comparacao);
-	
-    printf("Numeros dobrados:\n");
+    }
+
+
+    for (i = 0; i < 7 - 1; i++) {
+        for (e = 0; e < 7 - i - 1; e++) {
+            if (vetor[e] > vetor[e + 1]) {
+                temp = vetor[e];
+                vetor[e] = vetor[e + 1];
+                vetor[e + 1] = temp;
+            }
+        }
+    }
+
+    printf("Numeros dobrados em ordem decrescente:\n");
     for (i = 0; i < 7; i++) {
         printf("%d \n", vetor[i]);
     }
-    printf("\n");
-    
+
     return 0;
 }
 
